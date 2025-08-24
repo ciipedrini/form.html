6597a8ef144b19a79149a03868db10bfa975813b
Cibele Romanenghi Pedrini <ciipedrini@gmail.com>
2025-08-24 16:35:18 -0300

    login.js

--- login.js
+++ login.js
@@ -1,10 +1,12 @@
-var LerDadosEntrar = () =>{
+var LerDadosEntrar = () => {
     var email = document.getElementById("email").value;
     var senha = document.getElementById("senha").value;
-
-    if (email !== "" && senha !=="") {
-        alert("Login efetuado com sucesso!");}
-        else{
-            alert("Preencha todos os campos!");
-        }
+    const validarEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "" || senha === "") {
+        alert("Preencha todos os campos!");
+    } else if (!validarEmail.test(email)) {
+        alert("Digite um e-mail válido!");
+    } else {
+        alert("Login efetuado com sucesso!");
     }
+}
